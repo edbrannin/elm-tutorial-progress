fd72df330c88315bacc89d807f3df1fc5fdd3dce
Ed Brannin <edbrannin@gmail.com>
2016-02-04 22:34:12 -0500

    Counting clicks

--- Main.elm
+++ Main.elm
@@ -4,14 +4,17 @@ module Main (..) where
 import Html
 import Mouse
 
-view: Int -> Int -> Html.Html
-view x y =
-  Html.text ((toString x) ++ " " ++ (toString y))
+view: Int -> Int -> Int -> Html.Html
+view x y clicks =
+  Html.text ((toString x) ++ " " ++ (toString y) ++ " " ++ (toString clicks))
+
+countSignal =
+  Signal.foldp (\_ state -> state + 1) 0 Mouse.clicks
 
 main : Signal.Signal Html.Html
 
 main =
-  Signal.map2 view Mouse.x Mouse.y
+  Signal.map3 view Mouse.x Mouse.y countSignal
 
 
 
