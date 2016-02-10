96204feabe9d9ab5008737ba232f5c6b15561f8d
Ed Brannin <edbrannin@gmail.com>
2016-02-10 08:44:08 -0500

    Halfway through http://www.elm-tutorial.org/signals/mailboxes.html

--- Main.elm
+++ Main.elm
@@ -1,22 +1,20 @@
 module Main (..) where
 
-
 import Html
-import Mouse
 
-view: Int -> Int -> Int -> Html.Html
-view x y clicks =
-  Html.text ((toString x) ++ " " ++ (toString y) ++ " " ++ (toString clicks))
+view : String -> Html.Html
+view message =
+  Html.div
+    []
+    [
+      Html.div [] [ Html.text message ]
+    , Html.button [] [ Html.text "Click" ]
+    ]
 
-accu : () -> Int -> Int
-accu _ state = state + 1
+messageSignal =
+  Signal.constant "Hello"
 
-countSignal = Signal.foldp accu 0 Mouse.clicks
-
-main : Signal.Signal Html.Html
-
+main : Signal Html.Html
 main =
-  Signal.map3 view Mouse.x Mouse.y countSignal
-
-
+  Signal.map view messageSignal
 
