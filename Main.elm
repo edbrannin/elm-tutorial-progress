4f84df6d0db657932fe690deddc283afb0729d49
Ed Brannin <edbrannin@gmail.com>
2016-02-09 20:23:53 -0500

    Pulled the anonymous function to a named one just for practice

--- Main.elm
+++ Main.elm
@@ -8,8 +8,10 @@ view: Int -> Int -> Int -> Html.Html
 view x y clicks =
   Html.text ((toString x) ++ " " ++ (toString y) ++ " " ++ (toString clicks))
 
-countSignal =
-  Signal.foldp (\_ state -> state + 1) 0 Mouse.clicks
+accu : () -> Int -> Int
+accu _ state = state + 1
+
+countSignal = Signal.foldp accu 0 Mouse.clicks
 
 main : Signal.Signal Html.Html
 
